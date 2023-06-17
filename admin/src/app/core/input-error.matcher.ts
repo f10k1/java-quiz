149e1289a51ae51f75bccc40d4fe874cf86f7991
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-06-17 17:20:50 +0200

    Create input-error.matcher.ts

---
+++ admin/src/app/core/input-error.matcher.ts
@@ -0,0 +1,14 @@
+import {
+    FormControl,
+    FormGroupDirective,
+    NgForm,
+} from '@angular/forms';
+
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export default class FormErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        const isSubmitted = form && form.submitted;
+        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+}
