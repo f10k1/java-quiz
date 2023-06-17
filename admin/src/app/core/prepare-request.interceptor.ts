34c8141d05ce6820e1e6b5d939319737b4faf157
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-06-17 17:20:47 +0200

    Create prepare-request.interceptor.ts

---
+++ admin/src/app/core/prepare-request.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpContext, HttpContextToken, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AUTH_TOKEN } from "./services/auth.service";
+
+@Injectable()
+export default class PrepareInterceptor implements HttpInterceptor {
+
+    intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const headers: any = {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        };
+
+        if (httpRequest.context.get(AUTH_TOKEN)) headers["Authorization"] = "Bearer " + httpRequest.context.get(AUTH_TOKEN);
+
+        const preparedRequest = httpRequest.clone({ url: `http://localhost:8090/${httpRequest.url}`, setHeaders: headers });
+
+        console.log(preparedRequest);
+
+        return next.handle(preparedRequest);
+    }
+
+}
